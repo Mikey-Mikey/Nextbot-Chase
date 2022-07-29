05dc0e2bc59d5b176a31ef2e0b8513ac969ac23e
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 02:04:50 -0400

    Proximity Voice Fix

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -17,3 +17,18 @@ end )
 hook.Add( "PlayerSpawnSENT", "SpawnBlockSENT", function(ply)
 	return ply:IsSuperAdmin()
 end )
+
+hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
+    if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
+		return true
+	end
+	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() == OBS_MODE_NONE then
+		return true
+	end
+	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() == OBS_MODE_NONE then
+		return false
+	end
+	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
+		return false
+	end
+end )
