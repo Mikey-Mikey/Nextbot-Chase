439c0baa0d002fedc9c6c7908ef7da455c8ec101
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 15:28:54 -0400

    Spawn Fix 1

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -2,22 +2,6 @@ function GM:PlayerNoClip(ply, desiredState)
 	return false
 end
 
-hook.Add( "PlayerSpawnSWEP", "SpawnBlockSWEP", function(ply)
-	return ply:IsSuperAdmin()
-end )
-
-hook.Add( "PlayerSpawnVehicle", "SpawnBlockVehicle", function(ply)
-	return ply:IsSuperAdmin()
-end )
-
-hook.Add( "PlayerSpawnProp", "SpawnBlockProp", function(ply)
-	return ply:IsSuperAdmin()
-end )
-
-hook.Add( "PlayerSpawnSENT", "SpawnBlockSENT", function(ply)
-	return ply:IsSuperAdmin()
-end )
-
 hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
 	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
 		return true
