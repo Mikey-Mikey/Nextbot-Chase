0a348f1a4cdf4b062b9044997b5a76042b5402c5
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 15:51:50 -0400

    Weapomn

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -1,14 +1,7 @@
 function GM:PlayerNoClip(ply, desiredState)
 	return false
 end
-
-hook.Add( "OnEntityCreated", "PropScream", function( ent )
-	if ent:IsValid() and not table.HasValue(ents.FindByClass("npc_*"), ent) then
-		timer.Simple(0,function()
-			ent:Remove()
-		end)
-	end
-end )
+hook.Remove("OnEntityCreated", "PropScream")
 
 hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
 	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
