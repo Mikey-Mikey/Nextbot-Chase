9386807dc429fdc5330b66960ac6abd0e90672a5
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 15:35:18 -0400

    Player spawning stuff fix

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -2,6 +2,14 @@ function GM:PlayerNoClip(ply, desiredState)
 	return false
 end
 
+hook.Add( "OnEntityCreated", "PropScream", function( ent )
+	if ent:IsValid() and not table.HasValue(ents.FindByClass("npc_*"), ent) then
+		timer.Simple(0,function()
+			ent:Remove()
+		end)
+	end
+end )
+
 hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
 	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
 		return true
