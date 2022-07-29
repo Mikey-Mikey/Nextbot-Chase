574921fc6a96aba4ede68bdf050e514cc82f3327
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 15:57:21 -0400

    Weapon

--- shared.lua
+++ shared.lua
@@ -157,14 +157,12 @@ function GM:PlayerDisconnected(ply)
 	end
 end
 
-function GM:PlayerLoadout(ply)
-	if ply:GetObserverMode() == OBS_MODE_NONE then
-		ply:Give("parkourmod")
-	end
-end
 
 function GM:PlayerSpawn(ply)
 	has_people = true
+	if ply:GetObserverMode() == OBS_MODE_NONE then
+		ply:Give("parkourmod")
+	end
 	ply:SetVelocity(-ply:GetVelocity())
 	ply:SetModel( "models/player/odessa.mdl" )
 	timer.Simple(0,function()
