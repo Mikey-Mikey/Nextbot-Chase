3da3deccc1c0a0593970c78d2726749965492b31
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 02:12:22 -0400

    Fixed Spaced Tabs

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -19,7 +19,7 @@ hook.Add( "PlayerSpawnSENT", "SpawnBlockSENT", function(ply)
 end )
 
 hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
-    if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
+	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
 		return true
 	end
 	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() == OBS_MODE_NONE then
