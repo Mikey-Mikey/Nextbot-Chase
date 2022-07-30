f34f0ea3f7753dd2e94e8c3b636b2314c78aece7
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-29 20:38:30 -0400

    Disabled fall damage

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -2,6 +2,10 @@ function GM:PlayerNoClip(ply, desiredState)
 	return false
 end
 
+hook.Add( "GetFallDamage", "RealisticDamage", function( ply, speed )
+    return 0
+end )
+
 hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
 	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
 		return true
