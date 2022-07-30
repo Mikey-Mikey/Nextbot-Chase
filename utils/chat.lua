3ab179ec99797bea8415ad71c2adc01974f0262d
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-30 15:16:38 -0400

    Revert

--- utils/chat.lua
+++ utils/chat.lua
@@ -1,3 +1,5 @@
-function chatPrintFancy(msg,ply)
-    ply:SendLua("chat.AddText(Color(255, 255, 255), \"[\", Color(30, 255, 0), \"Nextbot Chase\", Color(255, 255, 255), \"] " .. msg .. "\")")
+function chatPrintFancy(msg)
+    for _, ply in pairs(player.GetAll()) do
+        ply:SendLua("chat.AddText(Color(255, 255, 255), \"[\", Color(30, 255, 0), \"Nextbot Chase\", Color(255, 255, 255), \"] " .. msg .. "\")")
+    end
 end
