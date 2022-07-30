4d60998a4d610c082993f85d0458e4e98c559a5c
Marshall_vak <Marshall_vak@outlook.com>
2022-07-30 12:33:05 -0400

    add spectate hook

--- cl_spectate.lua
+++ cl_spectate.lua
@@ -1,11 +1,11 @@
+-- internal spectating value
 local isSpectating = false
+
+-- micro optimizations
+local runHook = hook.Run
 
 -- listen to the server when it tells us if we are a spectator or not
 net.Receive("spectate", function()
     isSpectating = net.ReadBool()
+    runHook("spectate", isSpectating)
 end)
-
-
-
-
--- currently unused
