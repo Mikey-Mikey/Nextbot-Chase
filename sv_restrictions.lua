887d71df72ba1ed2e1815d735dedb52b5046f1a6
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-31 19:34:34 -0400

    Made it so regular players can hear spectators

--- sv_restrictions.lua
+++ sv_restrictions.lua
@@ -1,18 +1,3 @@
 hook.Add( "GetFallDamage", "RealisticDamage", function( ply, speed )
 	return 0
 end )
-
-hook.Add( "PlayerCanHearPlayersVoice", "Maximum Range", function( listener, talker )
-	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
-		return true
-	end
-	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() != OBS_MODE_NONE then
-		return true
-	end
-	if talker:GetObserverMode() == OBS_MODE_NONE and listener:GetObserverMode() == OBS_MODE_NONE then
-		return true
-	end
-	if talker:GetObserverMode() != OBS_MODE_NONE and listener:GetObserverMode() == OBS_MODE_NONE then
-		return false
-	end
-end )
