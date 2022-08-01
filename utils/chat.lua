48f0aabcd6989a8604af7bee4cda65cf7a48536a
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-31 20:35:54 -0400

    N word prevention

--- utils/chat.lua
+++ utils/chat.lua
@@ -3,3 +3,10 @@ function chatPrintFancy(msg)
         ply:SendLua("chat.AddText(Color(255, 255, 255), \"[\", Color(30, 255, 0), \"Nextbot Chase\", Color(255, 255, 255), \"] " .. msg .. "\")")
     end
 end
+
+hook.Add("PlayerSay","chat",function(ply,text,team)
+    if string.find(text,"nig") then
+        RunConsoleCommand("ulx ban",ply,1440)
+        return "******"
+    end
+end)
