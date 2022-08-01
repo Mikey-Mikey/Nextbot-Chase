ebe9ece3d21e6007b7e967229b6cf59d8bd5cb9f
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-08-01 12:04:08 -0400

    Fixed a chatprint issue

--- sv_spectate.lua
+++ sv_spectate.lua
@@ -112,7 +112,7 @@ end )
 
 hook.Add("PlayerUse", "players", function(ply,ent)
     if timer.Exists(tostring(ply) .. "cooldown") and ply:GetObserverMode() ~= OBS_MODE_NONE and ent:IsValid() then
-        ply:ChatPrint("You can't pick up props until", timer.TimeLeft(tostring(ply) .. "cooldown"), "seconds!")
+        ply:ChatPrint("You can't pick up props until " .. timer.TimeLeft(tostring(ply) .. "cooldown") .. " seconds!")
     end
     if ply:GetObserverMode() ~= OBS_MODE_NONE and ply.cooldown ~= true then
         if ply.cooldown ~= true and ent:IsValid() then
