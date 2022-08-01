3085d79f3c4f1ee1a21c48193a96b6e415b145c6
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-08-01 14:07:22 -0400

    fixed crash when spectating a player who is not alive

--- sv_spectate.lua
+++ sv_spectate.lua
@@ -84,7 +84,7 @@ hook.Add("KeyPress", "Spectate", function(ply, key)
     if key == IN_ATTACK --[[ or key == IN_ATTACK2]] then
         randomPly = GAMEMODE.players[random(1, #GAMEMODE.players)]
 
-        while ply:GetObserverTarget() == randomPly and #GAMEMODE.players > 1 and #player.GetAll() > 0 or not inTable(GAMEMODE.players, ply:GetObserverTarget()) do
+        while ply:GetObserverTarget() == randomPly and #GAMEMODE.players > 1 and #player.GetAll() > 0 and not inTable(GAMEMODE.players, ply:GetObserverTarget()) do
             randomPly = GAMEMODE.players[random(1, #GAMEMODE.players)]
         end
     end
