0057fd6f56b5ce12ae2c3486aca6e08d9c845b7f
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2022-07-31 22:00:49 -0400

    Put the censor in autorun on the server

--- utils/chat.lua
+++ utils/chat.lua
@@ -3,10 +3,3 @@ function chatPrintFancy(msg)
         ply:SendLua("chat.AddText(Color(255, 255, 255), \"[\", Color(30, 255, 0), \"Nextbot Chase\", Color(255, 255, 255), \"] " .. msg .. "\")")
     end
 end
-
-hook.Add("PlayerSay","chat",function(ply,text,team)
-    if string.find(text,"nigger") or string.find(text,"nigga") then
-        RunConsoleCommand("ulx", "banid", ply:SteamID(), 1440, "Being Racist, LAUGH AT THIS USER")
-        return false
-    end
-end)
